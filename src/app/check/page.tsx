64697e0305a5b0aa85072463501b486968a9dd2e
Niklas Ladehoff <business-mail-niklas@web.de>
2023-03-14 19:28:58 +0100

    added check page content

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -1,5 +1,40 @@
 import React from "react";
+import { Kufam } from "next/font/google";
 
+const kufam_bold = Kufam({
+  weight: "700",
+  style: "normal",
+  subsets: ["latin"],
+});
+const kufam = Kufam({ weight: "400", style: "normal", subsets: ["latin"] });
 export default function Check() {
-  return <div>page</div>;
+  return (
+    <div className="h-screen w-screen flex flex-row justify-center bg-[#222222]">
+      <div className="flex flex-col items-center pt-20">
+        <h1
+          className={`${kufam_bold.className} text-center text-[4rem] text-[#E2E2E2]`}
+        >
+          NameNation
+        </h1>
+        <div className="flex flex-col pt-20">
+          <label
+            className={`${kufam_bold.className} text-[1.25rem] text-[#E2E2E2]`}
+            htmlFor="search"
+          >
+            Your Name
+          </label>
+          <input
+            className="h-14 w-[35rem] rounded-xl bg-[#E2E2E2] shadow-lg"
+            id="search"
+            type="text"
+          />
+        </div>
+        <div className="w-[34rem]">
+          <ul className="">
+            <li className="">Test</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
