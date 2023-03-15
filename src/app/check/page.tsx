20549f10b7ab479f8dffa42438bdffbb6e63b892
Niklas Ladehoff <business-mail-niklas@web.de>
2023-03-15 19:59:28 +0100

    added loading spinner

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -16,6 +16,9 @@ export default function Check() {
   const [name, setName] = useState("");
   const [age, setAge] = useState(0);
   const [fact, setFact] = useState("");
+  const [loadingCountries, setLoadingCountries] = useState<
+    "waiting" | "loading" | "loaded"
+  >("waiting");
 
   const [countries, setCountries] = useState<Array<Country>>([]);
 
@@ -30,6 +33,8 @@ export default function Check() {
 
   const handleNameSubmit = (name: string) => {
     let lowerName = name.toLowerCase();
+
+    setLoadingCountries("loading");
 
     fetch(`https://api.nationalize.io?name=${lowerName}`)
       .then((res) => res.json())
@@ -52,7 +57,9 @@ export default function Check() {
       });
     }
     console.log(newData);
+
     setCountries(newData);
+    setLoadingCountries("loaded");
   };
 
   const transformPercentage = (probability: number) => {
@@ -73,8 +80,10 @@ export default function Check() {
 
   useEffect(() => {
     fetchFact();
+    setLoadingCountries("waiting");
 
     return () => {
+      setLoadingCountries("waiting");
       setFact("");
       setAge(0);
       setCountries([]);
@@ -93,33 +102,72 @@ export default function Check() {
           setName={(input: string) => setName(input)}
           handleNameSubmit={() => handleNameSubmit(name)}
         />
-        <NationWrapper>
-          {countries.map((country: Country, index) => (
-            <NationItem
-              index={index}
-              nation={transformCountryID(country.country_id)}
-              percentage={`${transformPercentage(country.probability)}`}
-              img_src={`https://flagcdn.com/w160/${country.country_id}.png`}
-              key={country.country_id}
-            />
-          ))}
-        </NationWrapper>
+        {loadingCountries !== "waiting" ? (
+          loadingCountries === "loaded" ? (
+            <>
+              {countries.length >= 1 ? (
+                <NationWrapper>
+                  {countries.map((country: Country, index) => (
+                    <NationItem
+                      index={index}
+                      nation={transformCountryID(country.country_id)}
+                      percentage={`${transformPercentage(country.probability)}`}
+                      img_src={`https://flagcdn.com/w160/${country.country_id}.png`}
+                      key={country.country_id}
+                    />
+                  ))}
+                </NationWrapper>
+              ) : (
+                <div
+                  className={`${kufam_bold.className} h-[29.95%] w-full flex flex-col items-center justify-center text-[1rem]  lg:text-[1.25rem]  text-[#D1D1D1] text-center`}
+                >
+                  Are you sure that&apos;s a name?
+                </div>
+              )}
+            </>
+          ) : (
+            <div className="h-[29.95%]  flex flex-col justify-center items-center">
+              <svg
+                aria-hidden="true"
+                className="w-14 h-14 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+            </div>
+          )
+        ) : (
+          <p
+            className={`${kufam_bold.className} h-[29.95%] flex flex-col items-center justify-center text-[1rem]  lg:text-[1.25rem]  text-[#D1D1D1] text-center`}
+          >
+            Enter a name to get started.
+          </p>
+        )}
         {age >= 1 && (
           <div
             className={`${kufam_bold.className} mt-10 h-20 w-1/2 py-2 px-3 rounded-xl text-[1rem] lg:text-[1.25rem] bg-[#222222]  lg:w-[32rem] text-[#D1D1D1] text-center flex flec-col items-center justify-center gap-1`}
           >
-            The age {age} has been connected to this name.
+            From your name you could be {age} years old.
           </div>
         )}
 
-        <div className="flex flex-col justify-center items-center mt-8 pb-4 w-full">
+        <div className="flex flex-col  justify-center items-center mt-8 pb-4 w-full">
           <h2
             className={`${kufam_bold.className} text-[1rem]  lg:text-[1.25rem]  text-[#D1D1D1] text-center`}
           >
             Fact for you
           </h2>
           <div
-            className={`${kufam.className} flex justify-center items-center lg:h-20 text-[1rem] bg-[#222222] w-1/2 lg:w-[32rem] h-fit text-[#D1D1D1] text-center rounded-lg  py-1 px-5`}
+            className={`${kufam.className} flex justify-center items-center lg:h-fit text-[1.1rem] bg-[#222222] w-1/2 lg:w-[32rem] h-fit text-[#D1D1D1] text-center rounded-lg  py-2 px-5`}
           >
             {fact}
           </div>
